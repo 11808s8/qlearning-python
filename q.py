3c265b70bebb8f154115c1a6f7d23af3ea4a0f0a
Adriano Gomes <adrianogsss@gmail.com>
2019-05-22 17:11:03 -0300

    Adição de invalidas e objetivo e iteração sobre json

--- q.py
+++ q.py
@@ -2,6 +2,30 @@ import json
 from colorama import Fore, Back, Style, init
 init()
 
+
+
+objetivo = {
+    "s50":100
+}
+invalidas ={
+"s7":-100,
+"s14":-100,
+"s10":-100,
+"s11":-100,
+"s20":-100,
+"s19":-100,
+"s24":-100,
+"s21":-100,
+"s30":-100,
+"s27":-100,
+"s31":-100,
+"s40":-100,
+"s39":-100,
+"s37":-100,
+"s41":-100
+}
+
+
 color_map = [i for i in range(1,51)]
 
 
@@ -24,3 +48,36 @@ def print_map():
         print(l)
 
 
+
+invalid = {}
+batata =True
+if batata:
+    print_map()
+else:
+    with open('mapa_inicial.recompensas_zeradas.json') as json_file:
+        q = json.load(json_file)
+        for key,value in q.items():
+            print(key)
+            for key1, value1 in value.items():
+                if(value1):
+                    for key2,value2 in value1.items():
+                        if(key2 in objetivo):
+                            value1.update({key2:objetivo[key2]})
+                            # value.update(value1)
+                            # q.update(value)
+                            # print(key2,value2)
+        print("pos")
+        for key,value in q.items():
+            
+            for key1, value1 in value.items():
+                if value1:
+                    for key2, value2 in value1.items():
+                        if key2 in objetivo:
+                            print(key)
+        # print(q["s"+str(49)])
+        # print_map()
+        # for key,value in q.items():
+        #     # print(key,value)
+        #     print(key)
+            # for key2, value2 in value.items():
+            #     print(key2, value2)
