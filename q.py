0178d8a92d2db6d5ac2050367da1fe901bbcfae9
Adriano Gomes <adrianogsss@gmail.com>
2019-06-11 20:10:40 -0300

    Aumento na quantidade de vezes que converge para valer

--- q.py
+++ q.py
@@ -327,19 +327,21 @@ color_map = [i for i in range(1,51)]
 # Valor para exibir informações de debugging
 DEBUG =False
 
+total_rodadas_cada_gama = list()
 
 # -------------------------------
 #Variáveis para testes
+# Separar os gamas por virgula
 tipos_de_gama = [0.1,0.2,0.8,0.9,0.95, 0.5 , 0.4, 0.3, 0.7, 0.6]
 porcentagem_escolha_maiores = 70
-total_rodadas_cada_gama = list()
+
 
 # Define se o programa executará até o fim dos N episódios definidos, sem parar quando converge
 roda_ate_o_fim = False
 
 # Define o gráfico será gerado com base na quantidade de passos do episódio executados ou 
 # na quantidade de execuções mesmo internas de quando está buscando o OBJETIVO
-salvar_por_episodio = False
+salvar_por_episodio = True
 episodios = 1000
 # Tipos aceitaveis de convergencia:
 # lista_otima
@@ -351,7 +353,7 @@ tipo_de_convergencia = 'lista_otima'
 gama = 0.5
 
 # Variável que define quantas vezes uma lista ficou sem mudar para ser convergência
-quando_converge = 5
+quando_converge = 100
 
 # Variável que define partindo de quantas execuções a lista de caminho ótimo passará a ser buscada
 quando_comeca_a_armazenar_lista_caminho_otimo = 5
